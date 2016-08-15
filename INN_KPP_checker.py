3afbe78ca38da9c1b1f67a580f06906c7b9c338f
HedgehogGrandpa <hedgehoggrandpa@gmail.com>
2016-08-15 13:51:18 +0500

    check input file existance corrected "try except"s

--- INN_KPP_checker.py
+++ INN_KPP_checker.py
@@ -1,8 +1,6 @@
 # -*- coding: cp1251 -*-
 import argparse
 import VATFormatter
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget
 
 
 def argument_parse():
@@ -13,34 +11,32 @@ def argument_parse():
                         help='Имя файла .xls или .xlsx формата с информацией об организациях')
     parser.add_argument('outputFileName', nargs='?', default='',
                         help='имя выходного файла')
-    parser.add_argument('-i', metavar='INN', dest='inn', action='store', nargs='*', type=int, required=True,
+    parser.add_argument('-i', metavar='INN', dest='inn', action='store', nargs='+', type=int, required=True,
                         help='Номера столбцов, в которых есть ИНН')
     parser.add_argument('-k', metavar='KPP', dest='kpp', action='store', nargs='*', type=int,
                         help='Номера столбцов, в которых есть КПП.\n'
                              'Номера столбцов указывать соответствующие номерам столбцов с ИНН')
+    parser.add_argument('-n', metavar='name_column', dest='name', nargs='*', action='store', type=int,
+                        help='Номера столбцов, используемых для формирования имени выходных файлов для КА\n'
+                             'столбцы будут скопированы и приписаны справа')
 
-    args = parser.parse_known_args()
-    args, er = args[0], args[1]
-    return (args.filename[0], args.outputFileName, args.inn, args.kpp), er
+    args = parser.parse_args()
+    return args.filename[0], args.outputFileName, args.inn, args.kpp, args.name
 
 
-def correcting(ifn, inns, kpps, ofn):
-    try:
-        formatter = VATFormatter.VATFormatter(ifn, inns, kpps, ofn)
-        formatter.correct_type_of_vat()
-    except Exception as e:
-        print(e)
+def correcting(ifn, inns, kpps, ofn, name):
+    formatter = VATFormatter.VATFormatter(ifn, inns, kpps, ofn, name)
+    formatter.correct_type_of_vat()
+
+
 
 def gui():
     pass
 
 
 def main():
-    (filename, output_file, inns, kpps), er = argument_parse()
-    gui() if er else correcting(filename, inns, kpps, output_file)
-
-    #formatter = VATFormatter.VATFormatter(filename, inns, kpps, output_file)
-    #formatter.correct_type_of_vat()
+    filename, output_file, inns, kpps, name = argument_parse()
+    correcting(filename, inns, kpps, output_file, name)
 
 
 if __name__ == '__main__':
