f1919eb8e14bd8541ec9dff2f186cdb21ec2cae1
HedgehogGrandpa <hedgehoggrandpa@gmail.com>
2016-08-31 13:06:58 +0500

    rename vat to inn

--- INN_KPP_checker.py
+++ INN_KPP_checker.py
@@ -1,38 +1,38 @@
-# -*- coding: cp1251 -*-
+# -*- coding: utf-8 -*-
 import argparse
-import VATFormatter
+import INNFormatter
 
 
 def argument_parse():
     parser = argparse.ArgumentParser(
-        description='Программа проверяет валидность ИНН-КПП, если возможно, то правит '
-                    '(пока только исправление ведущих нулей)')
+        description='РџСЂРѕРіСЂР°РјРјР° РїСЂРѕРІРµСЂСЏРµС‚ РІР°Р»РёРґРЅРѕСЃС‚СЊ РРќРќ-РљРџРџ, РµСЃР»Рё РІРѕР·РјРѕР¶РЅРѕ, С‚Рѕ РїСЂР°РІРёС‚ '
+                    '(РїРѕРєР° С‚РѕР»СЊРєРѕ РёСЃРїСЂР°РІР»РµРЅРёРµ РІРµРґСѓС‰РёС… РЅСѓР»РµР№)')
     parser.add_argument('filename', nargs=1,
-                        help='Имя файла .xls или .xlsx формата с информацией об организациях')
+                        help='РРјСЏ С„Р°Р№Р»Р° .xls РёР»Рё .xlsx С„РѕСЂРјР°С‚Р° СЃ РёРЅС„РѕСЂРјР°С†РёРµР№ РѕР± РѕСЂРіР°РЅРёР·Р°С†РёСЏС…')
     parser.add_argument('outputFileName', nargs='?', default='',
-                        help='имя выходного файла')
-    parser.add_argument('-i', metavar='INN', dest='inn', action='store', nargs='+', type=int, required=True,
-                        help='Номера столбцов, в которых есть ИНН')
+                        help='РёРјСЏ РІС‹С…РѕРґРЅРѕРіРѕ С„Р°Р№Р»Р°')
+    parser.add_argument('-i', metavar='INN_correcter', dest='inn', action='store', nargs='+', type=int, required=True,
+                        help='РќРѕРјРµСЂР° СЃС‚РѕР»Р±С†РѕРІ, РІ РєРѕС‚РѕСЂС‹С… РµСЃС‚СЊ РРќРќ')
     parser.add_argument('-k', metavar='KPP', dest='kpp', action='store', nargs='*', type=int,
-                        help='Номера столбцов, в которых есть КПП.\n'
-                             'Номера столбцов указывать соответствующие номерам столбцов с ИНН')
+                        help='РќРѕРјРµСЂР° СЃС‚РѕР»Р±С†РѕРІ, РІ РєРѕС‚РѕСЂС‹С… РµСЃС‚СЊ РљРџРџ.\n'
+                             'РќРѕРјРµСЂР° СЃС‚РѕР»Р±С†РѕРІ СѓРєР°Р·С‹РІР°С‚СЊ СЃРѕРѕС‚РІРµС‚СЃС‚РІСѓСЋС‰РёРµ РЅРѕРјРµСЂР°Рј СЃС‚РѕР»Р±С†РѕРІ СЃ РРќРќ')
     parser.add_argument('-n', metavar='name_column', dest='name', nargs='*', action='store', type=int,
-                        help='Номера столбцов, используемых для формирования имени выходных файлов для КА\n'
-                             'столбцы будут скопированы и приписаны справа')
+                        help='РќРѕРјРµСЂР° СЃС‚РѕР»Р±С†РѕРІ, РёСЃРїРѕР»СЊР·СѓРµРјС‹С… РґР»СЏ С„РѕСЂРјРёСЂРѕРІР°РЅРёСЏ РёРјРµРЅРё РІС‹С…РѕРґРЅС‹С… С„Р°Р№Р»РѕРІ РґР»СЏ РљРђ\n'
+                             'СЃС‚РѕР»Р±С†С‹ Р±СѓРґСѓС‚ СЃРєРѕРїРёСЂРѕРІР°РЅС‹ Рё РїСЂРёРїРёСЃР°РЅС‹ СЃРїСЂР°РІР°')
     parser.add_argument('-p', metavar='prefix', dest='pre', nargs='*', action='store', type=str,
-                        help='Номера столбцов и префиксы для добавления.\n'
-                             'Задавать в порядке <Номер_столбца1> <Префикс1> <Номер_столбца2> <Прификс2> ...')
+                        help='РќРѕРјРµСЂР° СЃС‚РѕР»Р±С†РѕРІ Рё РїСЂРµС„РёРєСЃС‹ РґР»СЏ РґРѕР±Р°РІР»РµРЅРёСЏ.\n'
+                             'Р—Р°РґР°РІР°С‚СЊ РІ РїРѕСЂСЏРґРєРµ <РќРѕРјРµСЂ_СЃС‚РѕР»Р±С†Р°1> <РџСЂРµС„РёРєСЃ1> <РќРѕРјРµСЂ_СЃС‚РѕР»Р±С†Р°2> <РџСЂРёС„РёРєСЃ2> ...')
     parser.add_argument('-s', metavar='suffix', dest='suf', nargs='*', action='store', type=str,
-                        help='Номера столбцов и суффиксы для добавления.\n'
-                             'Задавать в порядке <Номер_столбца1> <Суффикс1> <Номер_столбца2> <Суффикс2> ...')
+                        help='РќРѕРјРµСЂР° СЃС‚РѕР»Р±С†РѕРІ Рё СЃСѓС„С„РёРєСЃС‹ РґР»СЏ РґРѕР±Р°РІР»РµРЅРёСЏ.\n'
+                             'Р—Р°РґР°РІР°С‚СЊ РІ РїРѕСЂСЏРґРєРµ <РќРѕРјРµСЂ_СЃС‚РѕР»Р±С†Р°1> <РЎСѓС„С„РёРєСЃ1> <РќРѕРјРµСЂ_СЃС‚РѕР»Р±С†Р°2> <РЎСѓС„С„РёРєСЃ2> ...')
 
     args = parser.parse_args()
     return args.filename[0], args.outputFileName, args.inn, args.kpp, args.name, args.pre, args.suf
 
 
 def correcting(ifn, inns, kpps, ofn, name, pre, suf):
-    formatter = VATFormatter.VATFormatter(ifn, inns, kpps, ofn, name, pre, suf)
-    formatter.correct_vat()
+    formatter = INNFormatter.INNFormatter(ifn, inns, kpps, ofn, name, pre, suf)
+    formatter.correct_inn()
 
 
 def gui():
@@ -41,9 +41,8 @@ def gui():
 
 def main():
     filename, output_file, inns, kpps, name, pre, suf = argument_parse()
-    # correcting(filename, inns, kpps, output_file, name, pre, suf)
-    formatter = VATFormatter.VATFormatter(filename, inns, kpps, output_file, name, pre, suf)
-    formatter.correct_vat()
+    formatter = INNFormatter.INNFormatter(filename, inns, kpps, output_file, name, pre, suf)
+    formatter.correct_inn()
 
 
 if __name__ == '__main__':
